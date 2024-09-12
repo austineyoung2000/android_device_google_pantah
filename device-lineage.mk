db87608d36a64fa3c2495f3b193de86875b12407
Austin Young <austineyoung2000@gmail.com>
2024-09-11 20:48:24 -0400

    cheeteh: Ship BCR, Pixel Parts & V4A
    Change-Id: Ie3f99c78a05b4ee0c875b82135eccfdcb64b0450

--- device-lineage.mk
+++ device-lineage.mk
@@ -21,6 +21,18 @@ PRODUCT_PACKAGES += \
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/pantah-kernel/Image.lz4
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
+# Basic Call Recorder
+include vendor/bcr/bcr.mk
+
+# Viper4AndroidFx
+include packages/apps/ViPER4AndroidFX/config.mk
+
+# Face Unlock
+-include vendor/google/faceunlock/device.mk
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
