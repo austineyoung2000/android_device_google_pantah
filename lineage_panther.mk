40bf7ccffd212e67494a3ee1a242d78516d50d1d
Austin Young <austineyoung2000@gmail.com>
2024-10-30 13:11:22 -0400

    pantah: Removed Flags

--- lineage_panther.mk
+++ lineage_panther.mk
@@ -30,14 +30,8 @@ TARGET_HAS_UDFPS := true
 # Gms 
 WITH_GMS := true
 
-# Ship Pixel Launcher
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-
 # Ship Google Camera
 TARGET_PREBUILT_GOOGLE_CAMERA := true
-
-# Lawnchair Prebuilt
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 
 # disable/enable blur support, default is false
 TARGET_ENABLE_BLUR := true
