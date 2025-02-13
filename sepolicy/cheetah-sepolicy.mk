ca86a0a92e77a9975c6d59eff87040e7de4e93c6
Austin Young <austineyoung2000@gmail.com>
2025-02-12 21:28:01 -0500

    pantah: Shipping Google FaceUnlock & V4A

--- sepolicy/cheetah-sepolicy.mk
+++ sepolicy/cheetah-sepolicy.mk
@@ -1,3 +1,6 @@
 # sepolicy that are shared among devices using whitechapel
 BOARD_SEPOLICY_DIRS += device/google/pantah/sepolicy/cheetah
 
+# audio
+BOARD_SEPOLICY_DIRS += device/google/pantah/sepolicy/audio
+
--- sepolicy/panther-sepolicy.mk
+++ sepolicy/panther-sepolicy.mk
@@ -1,2 +1,5 @@
 # sepolicy that are shared among devices using whitechapel
 BOARD_SEPOLICY_DIRS += device/google/pantah/sepolicy/panther
+
+# audio
+BOARD_SEPOLICY_DIRS += device/google/pantah/sepolicy/audio
